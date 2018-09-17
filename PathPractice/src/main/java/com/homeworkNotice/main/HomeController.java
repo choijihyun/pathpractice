4b9922d3120cef1d1457bb4b74178c6d00eef868
최지현 <chlwlgus119@gmail.com>
2018-09-17 11:47:43 +0900

    add homeworkcontroller

--- PathPractice/src/main/java/com/homeworkNotice/main/HomeController.java
+++ PathPractice/src/main/java/com/homeworkNotice/main/HomeController.java
@@ -28,18 +28,6 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
     	System.out.println("server start");
-    	/*
-    	 * 헷갈릴 때 공부용으로 써~~~~~~~~~~~~~~
-    	List<UserDto> userDtoList=userDao.selectAllList();
-    	for(UserDto userDto : userDtoList) {
-    		System.out.println(userDto.getId());
-    	}
-    	List<HomeworkDto> homeworkDtoList=homeworkDao.selectAllList();
-    	for(HomeworkDto homeworkDto : homeworkDtoList) {
-    		System.out.println(homeworkDto.getId());
-    	}
-    	 * 헷갈릴 때 공부용으로 써~~~~~~~~~~~~~~
-    	*/
         return "index";
     }
     
@@ -56,6 +44,57 @@ public class HomeController {
     	System.out.println("join!");
         return "join";
     }
+    
+    @RequestMapping(value = "/assignment_add", method = RequestMethod.GET)
+    public String assignment_add(Locale locale, Model model) {
+    	System.out.println("assignment_add!");
+        return "assignment_add";
+    }
+    
+    @RequestMapping(value = "/assignment", method = RequestMethod.GET)
+    public String assignment(Locale locale, Model model) {
+    	System.out.println("assignment!");
+        return "assignment";
+    }
+    
+    @RequestMapping(value = "/changePW", method = RequestMethod.GET)
+    public String changePW(Locale locale, Model model) {
+    	System.out.println("changePW!");
+        return "changePW";
+    }
+    
+    @RequestMapping(value = "/find_subject", method = RequestMethod.GET)
+    public String find_subject(Locale locale, Model model) {
+    	System.out.println("find_subject!");
+        return "find_subject";
+    }
+    
+    @RequestMapping(value = "/findPW", method = RequestMethod.GET)
+    public String findPW(Locale locale, Model model) {
+    	System.out.println("findPW!");
+        return "findPW";
+    }
+    
+    @RequestMapping(value = "/menu", method = RequestMethod.GET)
+    public String menu(Locale locale, Model model) {
+    	System.out.println("menu!");
+        return "menu";
+    }
+    
+    @RequestMapping(value = "/mypage", method = RequestMethod.GET)
+    public String mypage(Locale locale, Model model) {
+    	System.out.println("mypage!");
+        return "mypage";
+    }
+    
+    @RequestMapping(value = "/timetable_page", method = RequestMethod.GET)
+    public String timetable_page(Locale locale, Model model) {
+    	System.out.println("timetable_page!");
+        return "timetable_page";
+    }
+    
+    
+    
     
     
 
