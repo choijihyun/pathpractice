a652d8f18473ef90fe4ecb8689328d725b16ed5f
최지현 <chlwlgus119@gmail.com>
2018-10-28 21:44:08 +0900

    로그인하면 아이디 비번 정보 저장한거 불러와서 공지사항 긁어오는 스크립트

--- PathPractice/src/main/webapp/WEB-INF/get_info.py
+++ PathPractice/src/main/webapp/WEB-INF/get_info.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
@@ -5,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
 
 
 def get_info(uid, upw):
@@ -24,20 +27,34 @@ def get_info(uid, upw):
     driver.get('https://blackboard.sejong.ac.kr')
     driver.implicitly_wait(3)
     driver.find_element_by_id('toggle_login_form').click()
-
+    
     driver.find_element_by_name('user_id').send_keys(stuid)
-    sleep(2)
-    alert = driver.switch_to_alert()
-    alert.accept()
+    try:
+        alert = browser.switch_to_alert()
+        alert.accept()
+        print ("alert accepted")
+    except:
+        print ("no alert")
+
+    print("후엥")
     driver.find_element_by_name('password').send_keys(pw)
+    try:
+        alert = browser.switch_to_alert()
+        alert.accept()
+        print ("alert accepted")
+    except:
+        print ("no alert")
 
     driver.implicitly_wait(3)
-    alert = driver.switch_to_alert()
-    alert.accept()
     driver.find_element_by_id("entry-login").click()
+    try:
+        alert = browser.switch_to_alert()
+        alert.accept()
+        driver.find_element_by_id("entry-login").click()
+        print ("alert accepted")
+    except:
+        print ("no alert")
 
-    alert = driver.switch_to_alert()
-    alert.accept()
 
   #공지사항 찾아가기
     driver.get('https://blackboard.sejong.ac.kr/webapps/streamViewer/streamViewer?cmd=view&streamName=alerts&globalNavigation=false')
@@ -65,9 +82,19 @@ def get_info(uid, upw):
 
     f.close()
     driver.quit()
-      
 
-        
+f = open('userInfo.txt','r', encoding='UTF8')
 
-    
+a=[]
+while True:
+    line = f.readline()
+    if not line: break
+    a.append(line)
+
+stuid =a[0].rstrip("\r\n")
+pw =a[1]
+print(stuid, pw)
+get_info(stuid, pw)
+
+f.close()
 
